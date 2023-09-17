21f928bde9183994d3e3ce85f583900f9344f25c
Amber Routella <rautela.amber@gmail.com>
2023-09-17 23:09:13 +0530

    commented some part of code

--- devops_terraform/Terraform/main.tf
+++ devops_terraform/Terraform/main.tf
@@ -11,10 +11,10 @@ resource "aws_s3_bucket" "new_bucket" {
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "hosting_to_bucket" {
-  bucket = "aws_s3_bucket.new_bucket.id"
+# resource "aws_s3_bucket_website_configuration" "hosting_to_bucket" {
+#   bucket = "aws_s3_bucket.new_bucket.id"
   
- index_document {
-    suffix = "index.html"
-  }
-}
+#  index_document {
+#     suffix = "index.html"
+#   }
+# }
--- devops_terraform/Terraform/output.tf
+++ devops_terraform/Terraform/output.tf
@@ -1,4 +1,4 @@
-output "website_url" {
-  description = "URL of the deployed website"
-  value       = "http://${aws_s3_bucket_website_configuration.hosting_to_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
-}
+# output "website_url" {
+#   description = "URL of the deployed website"
+#   value       = "http://${aws_s3_bucket_website_configuration.hosting_to_bucket.bucket}.s3-website-${var.region}.amazonaws.com"
+# }
